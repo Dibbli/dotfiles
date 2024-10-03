c6ecf024dc96d50a27d48e152bb937e697333f33
Dibbli <botond@dibbli.net>
2024-10-03 12:51:06 +0200

    fixed eslint attempting to lint git blame
    kind of a fake fix, might cause problem later, it's probably fine :)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -561,6 +561,12 @@ lspconfig.eslint.setup({
 			command = "EslintFixAll",
 		})
 	end,
+	handlers = {
+    ["textDocument/publishDiagnostics"] = vim.lsp.with(
+        vim.lsp.diagnostic.on_publish_diagnostics,
+        { virtual_text = false, signs = true, update_in_insert = false, underline = true}
+    ),
+  }
 })
 
 lspconfig.tailwindcss.setup({
