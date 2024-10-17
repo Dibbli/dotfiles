1f5f397f214799e79fed690cb8ae2f4797dfdd25
Dibbli <botond@dibbli.net>
2024-10-17 13:46:34 +0200

    deleted friendlysnippets and added some more of my own

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -439,7 +439,6 @@ require("lazy").setup({
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-vsnip",
 			"hrsh7th/vim-vsnip",
-			"rafamadriz/friendly-snippets",
 			"onsails/lspkind.nvim",
 		},
 		config = function()
